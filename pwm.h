9daf9c62a2f046a81151b55058dd79e41e704af8
smk12 <smk12@email.arizona.edu>
2015-11-09 14:15:30 -0700

    Create pwm.h

---
+++ pwm.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   pwm.h
+ * Author: gvanhoy
+ *
+ * Created on October 8, 2015, 9:14 AM
+ */
+
+#ifndef PWM_H
+#define	PWM_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void initPWM();
+void flipFlop(int direction); 
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PWM_H */
