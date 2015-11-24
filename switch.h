28d3ecfeae675947aa1d6e5b95d5144b59c5ecd7
smk12 <smk12@email.arizona.edu>
2015-11-23 20:09:05 -0700

    Create switch.h

---
+++ switch.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   switch.h
+ * Author: Sean Kenney
+ *
+ * Created on August 27, 2015, 3:12 PM
+ */
+
+#ifndef SWITCH_H
+#define	SWITCH_H
+
+#define SW1 PORTDbits.RD6;
+
+void initSwitches();
+
+#endif	/* SWITCH_H */
+
